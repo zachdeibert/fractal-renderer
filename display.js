5f21c79fb6865278f52d00077bdea6c3fd2647cf
Zach Deibert <zachdeibert@gmail.com>
2017-11-25 12:45:17 -0600

    Workaround for broken video encoding

--- display.js
+++ display.js
@@ -3,6 +3,8 @@ window.addEventListener("load", function() {
     var ctx = canvas.getContext("2d");
     var keyframe = new Uint8Array([ 4 ]).buffer;
     var frame = new Uint8Array([ 5 ]).buffer;
+    var frameCounter = 0;
+    var keyFrameRatio = 20;
 
     function init() {
         var ws = new WebSocket("ws://" + location.host);
@@ -15,12 +17,12 @@ window.addEventListener("load", function() {
             var header = new Uint32Array(ev.data.slice(1, 17));
             if (header[2] == 0 || header[3] == 0) {
                 setTimeout(function() {
-                    ws.send(frame);
+                    ws.send(++frameCounter % keyFrameRatio == 0 ? keyframe : frame);
                 }, 1000 / 60);
             } else {
                 var image = new ImageData(new Uint8ClampedArray(ev.data, 17, header[2] * header[3] * 4), header[2], header[3]);
                 ctx.putImageData(image, header[0], header[1]);
-                ws.send(frame);
+                ws.send(++frameCounter % keyFrameRatio == 0 ? keyframe : frame);
             }
         });
         ws.addEventListener("error", function(ev) {
